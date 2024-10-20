ea1e4fc70a557cf5ccf8bc2c32b201ab4921d209
FMTLVZGIT <thomas.fmtlvz@gmail.com>
2024-10-20 02:03:36 -0300

    Update Rpg•levelup.js

--- plugins/Rpg•levelup.js
+++ plugins/Rpg•levelup.js
@@ -2,7 +2,7 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-let img = await (await fetch(`https://qu.ax/DwRoz.jpg`)).buffer()
+let img = await (await fetch(`https://qu.ax/gUMgG.jpg`)).buffer()
 let name = conn.getName(m.sender)
 let user = global.db.data.users[m.sender]
 if (!canLevelUp(user.level, user.exp, global.multiplier)) {
